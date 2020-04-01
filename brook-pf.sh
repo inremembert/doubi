2c4d0ee2ae05b54606f92c0d74a112c6696ba3d5
inremembert <19601596+inremembert@users.noreply.github.com>
2020-04-01 13:44:06 +0800

    Update brook-pf.sh

--- brook-pf.sh
+++ brook-pf.sh
@@ -71,7 +71,7 @@ check_new_ver(){
 # 	if [[ "${Download_type}" == "1" ]]; then
 # 		brook_new_ver=$(v20200201)
 # 		brook_new_ver=$$(wget -qO- https://api.github.com/repos/txthinking/brook/releases| grep "tag_name"| head -n 1| awk -F ":" '{print $2}'| sed 's/\"//g;s/,//g;s/ //g')
-	else
+# 	else
 # 		brook_new_ver=$(v20200201)
 # 	fi
 # 	if [[ -z ${brook_new_ver} ]]; then
